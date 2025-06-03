0df4bd368d39293d99b3dd92a00691f0a20cf007
Vsevolod Cherepashkin <cherepashkin0@gmail.com>
2025-06-03 14:54:15 +0000

    phik works also with categorical columns

--- mycoding/main.py
+++ mycoding/main.py
@@ -10,6 +10,8 @@ from sklearn.model_selection import train_test_split
 from phik import phik_matrix
 import numpy as np
 from pathlib import Path
+pd.options.display.max_rows = None
+pd.set_option('display.max_rows', None)
 
 
 project_id = os.environ.get('GCP_PROJECT_ID')
@@ -32,12 +34,10 @@ def plots2pdf(plots, fname):
         for plot in plots:
            pp.savefig(plot.figure)
 
-def histogram_create():
+def histogram_create(dct_cols):
     print(describe_df['Role'].value_counts())
     client = bigquery.Client(project=project_id)
-    dct_cols = {'num': describe_df.loc[describe_df['Role'] == 'num', 'Column_Name'].tolist(),
-                'cat': describe_df.loc[describe_df['Role'] == 'cat', 'Column_Name'].tolist(),
-                'hot': describe_df.loc[describe_df['Role'] == 'hot', 'Column_Name'].tolist()}
+    
 
     print('Num cols:', dct_cols['num'])
     print('Cat not hot cols:', dct_cols['cat'])
@@ -47,7 +47,7 @@ def histogram_create():
             SELECT {", ".join(dct_cols['num'])+", Cancelled"}
             FROM `{table_id}`
             ORDER BY RAND()
-            LIMIT 100000
+            LIMIT {SAMPLE_SIZE}
             """
     df = client.query(query).to_dataframe(create_bqstorage_client=True)
     lst_of_histograms = []
@@ -57,14 +57,13 @@ def histogram_create():
         lst_of_histograms.append(ax)
         plt.close(fig)
     plots2pdf(lst_of_histograms, os.path.join(today_dir, 'histogram_nums.pdf'))
-    return dct_cols
 
 
 def train_test_split_create(dct_cols):
     all_columns = [col for cols in dct_cols.values() for col in cols]    
     client = bigquery.Client(project=project_id)
     query = f"""
-            SELECT {", ".join(all_columns) + ", Cancelled"}
+            SELECT {", ".join(all_columns)}
             FROM `{table_id}`
             LIMIT {SAMPLE_SIZE}
             """
@@ -78,26 +77,54 @@ def train_test_split_create(dct_cols):
     y_test = df[['Cancelled']].iloc[test_idx]
     return X_train, X_test, y_train, y_test
 
-
-def phik_create_matrix(X_train, y_train, dct_cols):
-    skip_cols = describe_df.loc[describe_df['Skip_reason_phik'].isin(['id', 'high_cardinality']), 'Column_Name'].tolist()
-    all_columns = [col for cols in dct_cols.values() for col in cols]
-    df = pd.concat([X_train.reset_index(drop=True), y_train.reset_index(drop=True)], axis=1)
-    df = df.drop(columns=skip_cols)
+def drop_skip_cols(df, skip_cols, dct_cols):
+    existing_cols = [col for col in skip_cols if col in df.columns]
+    df = df.drop(columns=existing_cols)
     filtered_dct_cols = {}
     for key, cols in dct_cols.items():
         filtered_dct_cols[key] = [col for col in cols if col not in skip_cols]
-    for col in filtered_dct_cols['num']:
-        df[col] = pd.to_numeric(df[col], errors='coerce')
+    return df, filtered_dct_cols
+
+def phik_create_matrix(X_train, y_train, dct_cols):
+    skip_cols = describe_df.loc[describe_df['Skip_reason_phik'].notna(), 'Column_Name'].tolist()
+    df = pd.concat([X_train.reset_index(drop=True), y_train.reset_index(drop=True)], axis=1)
+    # print(89, 'df columns', df.columns)
+    df[['Cancelled', 'Diverted']] = df[['Cancelled', 'Diverted']].astype('Int8')
+    df, filtered_dct_cols = drop_skip_cols(df, skip_cols, dct_cols)
+    # for col in filtered_dct_cols['num']:
+    #     df[col] = pd.to_numeric(df[col], errors='coerce')
+    df[filtered_dct_cols['num']] = df[filtered_dct_cols['num']].astype(float)
+    cat_cols = filtered_dct_cols['cat'] + filtered_dct_cols['hot']
+    df[cat_cols] = df[cat_cols].astype('category')
+    # print('Columns before phik correlation \n', df.dtypes)
+    print(df[filtered_dct_cols['num']].dtypes)
     phik_corr = phik_matrix(df, interval_cols=filtered_dct_cols['num'], dropna=True, verbose=1)
-    cancelled_corr = phik_corr['Cancelled'].sort_values(ascending=False)
-    phik_corr.to_csv(f"{today_dir}/phik_correlation_matrix_{SAMPLE_SIZE:_}.csv")
+        # interval_cols=filtered_dct_cols['num'], 
+    # phik_long = phik_corr.stack().reset_index()
+    # phik_long.columns = ['column_a', 'column_b', 'value']
+    # phik_long = phik_long[phik_long['column_a'] != phik_long['column_b']]
+    # phik_long = 
 
 
-def main():   
-    num_cols = histogram_create()
-    X_train, X_test, y_train, y_test = train_test_split_create(num_cols)
-    phik_create_matrix(X_train, y_train, num_cols)
+def train(X_train, y_train, dct_cols):
+    # skip_cols = describe_df.loc[describe_df['Skip_reason_phik'] or describe_df['Phik_high_target'], 'Column_Name'].tolist()
+
+    # X_train, filtered_dct_cols = drop_skip_cols(X_train, skip_cols, dct_cols)
+    # X_test, filtered_dct_cols = drop_skip_cols(X_test, skip_cols, dct_cols)
+    # X_train[['Diverted']] = df[['Diverted']].astype('int8')
+    # X_test[['Diverted']] = 
+    pass
+
+
+
+def main():
+    dct_cols = {}
+    for key in ['num', 'cat', 'hot', 'tgt', 'dat']:
+        dct_cols[key] = describe_df.loc[describe_df['Role'] == key, 'Column_Name'].sort_values().tolist()   
+    # histogram_create(dct_cols)
+    X_train, X_test, y_train, y_test = train_test_split_create(dct_cols)
+    phik_create_matrix(X_train, y_train, dct_cols)
+    train(X_train, y_train, dct_cols)
 
 if __name__ == "__main__":
     today_date = datetime.today().strftime('%Y-%m-%d')
